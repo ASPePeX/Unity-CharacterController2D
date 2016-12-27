ccfd056ed7b864976162f161967b241b6a7b1d90
Alexander Scheurer <mail@aspepex.net>
2016-12-27 22:55:46 +0100

    Initial commit

---
+++ Assets/CameraController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public bool FollowPlayerY;
+    public float FollowYat;
+
+    private Transform _player;
+    private Vector3 _position;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    _player = GameObject.FindGameObjectWithTag("Player").transform;
+
+	    _position = new Vector3();
+
+	    if (FollowPlayerY)
+	    {
+	        _position.x = _player.position.x;
+	        _position.y = _player.position.y;
+	        _position.z = transform.position.z;
+	    }
+	    else
+	    {
+	        _position.x = _player.position.x;
+	        _position.y = FollowYat;
+	        _position.z = transform.position.z;
+	    }
+
+	    transform.position = _position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (FollowPlayerY)
+	    {
+	        _position.x = _player.position.x;
+	        _position.y = _player.position.y;
+	    }
+	    else
+	    {
+	        _position.x = _player.position.x;
+	    }
+	}
+}
---
+++ Assets/CharController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class CharController : MonoBehaviour
+{
+    private Rigidbody2D _rig;
+
+    private Vector2 _forceVector;
+    private bool _grounded;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    _rig = GetComponent<Rigidbody2D>();
+	    _forceVector = new Vector2();
+	}
+	
+	// Update is called once per frame
+    void Update()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down);
+        if (hit.distance < 0.05f)
+        {
+            _grounded = true;
+        }
+        else
+        {
+            _grounded = false;
+        }
+
+        _forceVector.x = Input.GetAxis("Horizontal") * 10;
+
+        if (_grounded)
+        {
+            _forceVector.y = Input.GetAxis("Jump") * 400;
+        }
+        else
+        {
+            _forceVector.y = 0;
+        }
+
+        _rig.AddForce(_forceVector);
+
+
+    }
+}
